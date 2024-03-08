d5456a41a5dbe6ea3623fcfdc4c7d54d54a7ef2b
MaryKilewe <marykilewe@gmail.com>
2024-03-08 09:20:09 +0300

    added test site

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -11,6 +11,9 @@ if (window.location.host === "localhost:3000"){
 }else if (window.location.host === "data.kenyahmis.org:3838"){
     client_id = "dwh.his"
     authority = 'https://auth.kenyahmis.org/dwhidentity'
+}else if (window.location.host === "histest.kenyahmis.org"){
+    client_id = "dwh.his"
+    authority = 'https://auth.kenyahmis.org/dwhidentity'
 }else if (window.location.host === "prod.kenyahmis.org:3001"){
     client_id = "dwh.his-prod"
     authority = 'https://identity.kenyahmis.org'
